408053ba639e97f7fbf2ce567275d115801a7ece
skhetcho <khetcho@gmail.com>
2025-05-23 22:03:38 -0700

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,16 @@
 import type { Metadata } from 'next'
 import './globals.css'
+// <meta name="apple-mobile-web-app-title" content="Paperhead" />
+
 
 export const metadata: Metadata = {
   title: 'Paperhead',
   description: 'Paperhead | build defi project live on stream',
   generator: 'Paperhead',
+  icons: {
+    icon: '/favicon.ico',
+    apple: '/favicon.ico',
+  }
 }
 
 export default function RootLayout({
